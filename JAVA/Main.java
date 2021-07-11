b7e97e27c6974c1e524e131ca721a12aa4cc7a4d
Erik Daniel <erikrame69@gmail.com>
2021-07-11 15:05:04 -0500

    Dando vida a nuestras clases en Java y Python

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -2,6 +2,23 @@ package JAVA;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");        
+        System.out.println("Hola mundo, estoy aprendiendo POO");
+        Car car = new Car();
+        car.license = "EDR399";
+        car.driver = "Daniel Mendoza";
+        car.passegenger = 7;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "RME990";
+        car2.driver = "Erik Ramirez";
+        car2.passegenger = 3;
+        car2.printDataCar();
+
+        Car car3 = new Car();
+        car3.license = "EDR903";
+        car3.driver = "Daniel Ramirez";
+        car3.passegenger = 3;
+        car3.printDataCar();
     }
-}
+} 
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from car import Car
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license= "ACC021"
+    car.driver = "Juan Martinez"
+    car.driver = "Andres Herrera"
+    print(vars(car)) 
