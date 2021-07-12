c228023ce1fb45d6cfd798e59a9d937344cbb15b
Erik Daniel <erikrame69@gmail.com>
2021-07-12 01:39:14 -0500

    Encapsulando atributos en Java

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -4,15 +4,34 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    private Integer passegenger;
+    private Integer passenger;
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
-        System.out.println("passegenger:" +passegenger);
+        
     }
 
     void printDataCar() {
-        System.out.println("Car License: " + license +  "  Name Driver: " + driver.name);
+        if(passenger != null){
+            System.out.println("Car License: " + license +  "  Name Driver: " + driver.name + " Passenger: " + passenger);
+        }
+        
     }
+
+    public Integer getPassenger(){
+        return passenger;
+    }
+    public void setPassenger(Integer passenger){
+        if(passenger == 4){
+         this.passenger = passenger;
+         System.out.println("Bienvenido, Viajemos seguros con Uber =)");
+        }
+        else { 
+            System.out.println("Oh oh, Parece que hacen falta más acompañantes :(");
+        }
+    }
+
+
+
 }
--- JAVA/Main.java
+++ JAVA/Main.java
@@ -3,16 +3,19 @@ package JAVA;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo, estoy aprendiendo POO");
-        Car car = new Car("DMR399", new Account("Daniel Mendoza", "DMID123"));
-       // car.passegenger = 7;
-        car.printDataCar();
+        UberX uberX =new UberX("DMR399", new Account("Daniel Mendoza", "DMID123"), "Hyundai", "i10");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
+        //uberX.passenger = 4;
+        // car.passegenger = 7;
+        //car.printDataCar();
 
        /* Car car2 = new Car("RME990", new Account("Erik Ramirez", "ERID303"));
-        car2.passegenger = 3;
+        car2.passenger = 3;
         car2.printDataCar();
 
         Car car3 = new Car("ALR903", new Account("Alejandro Ramirez", "ARID909"));
-        car3.passegenger = 3;
+        car3.passenger = 3;
         car3.printDataCar();*/
     }
 } 
