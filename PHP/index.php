8bdb0737a4b6fad914c98a0bc550376deb4252c2
Erik Daniel <erikrame69@gmail.com>
2021-07-11 19:12:00 -0500

    Primer prueba SERPHP

--- PHP/index.php
+++ PHP/index.php
@@ -3,10 +3,14 @@ require_once("car.php");
 require_once("uberX.php");
 require_once("uberPool.php");
 require_once("account.php");
+require_once("uberBlack.php")
 
-$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX = new uberX("DMR399", new Account("Daniel Mendoza", "DMID123"), "Chevrolet", "Aveo");
 $uberX->printDataCar();
 
-$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
+$uberPool = new uberPool("RME990", new Account("Erik Ramirez", "ERID303"), "Dodge", "Attitude");
+$uberPool->printDataCar();
+
+$uberPool = new uberBlack("ALR903", new Account("Alejandro Ramirez", "ARID909"), "Porche", "911 carrera s");
 $uberPool->printDataCar();
 ?>
